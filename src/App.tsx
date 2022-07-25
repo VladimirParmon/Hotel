c83527771660af00c38e7dadae2c6b059fcd1e40
VladimirParmon <parmon_vladimir@bk.ru>
2022-07-25 16:09:12 +0300

    set up first routing (welcome page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./app/layouts/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "./app/pages/welcomePage";
+import { IFilters, PaletteMode } from "./app/constants/models";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { getDesignTokens } from "./app/material/theme";
+import { defaultFilters } from "app/constants/constants";
+
+const ColorModeToggleContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
-  return <h1>App works!</h1>;
+  const [themeMode, setThemeMode] = useState<PaletteMode>("light");
+  const [currentFilters, setCurrentFilters] = useState<IFilters>(defaultFilters);
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setThemeMode((prevMode: PaletteMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+  const theme = React.useMemo(() => createTheme(getDesignTokens(themeMode)), [themeMode]);
+  return (
+    <React.Fragment>
+      <ColorModeToggleContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<WelcomePage filters={currentFilters} />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ColorModeToggleContext.Provider>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/app/constants/enums.ts
@@ -0,0 +1,17 @@
+export enum NavigationOptions {
+  APARTMENTS = "Apartments",
+  SERVICES = "Services",
+  OFFERS = "Special offers",
+  JOBS = "Job opportunities",
+  UPDATES = "Updates",
+  TERMS = "Terms of service",
+}
+
+export enum NavigationLinks {
+  APARTMENTS = "/apartments",
+  SERVICES = "/services",
+  OFFERS = "/special-offers",
+  JOBS = "/job-opportunities",
+  UPDATES = "/updates",
+  TERMS = "/terms-of-service",
+}
