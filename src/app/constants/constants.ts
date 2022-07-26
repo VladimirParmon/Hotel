d8a3c50b7e3dacc9dd3d17859d8849d7cd6a5305
VladimirParmon <parmon_vladimir@bk.ru>
2022-07-26 18:40:49 +0300

    add new constants for choices component

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,12 @@
-import { NavigationLinks, NavigationOptions } from "./enums";
+import {
+  ChoicesSections,
+  Choices_Accessability,
+  Choices_Conveniences,
+  Choices_Terms,
+  NavigationLinks,
+  NavigationOptions,
+} from "./enums";
+import { ChoicesDescription } from "./models";
 
 export const oneDayMs = 86_000_000;
 
@@ -26,5 +34,44 @@ export const navigationOptions = [
   {
     name: NavigationOptions.TERMS,
     link: NavigationLinks.TERMS,
+  },
+];
+
+export const choices: ChoicesDescription[] = [
+  {
+    sectionName: ChoicesSections.CONVENIENCES,
+    choices: [
+      { name: Choices_Conveniences.BALCONY },
+      { name: Choices_Conveniences.CONDITIONER },
+      { name: Choices_Conveniences.FIREPLACE },
+      { name: Choices_Conveniences.INSULATION },
+      { name: Choices_Conveniences.WIFI },
+      { name: Choices_Conveniences.WINDOWS },
+      { name: Choices_Conveniences.WORKSPACE },
+    ],
+  },
+  {
+    sectionName: ChoicesSections.TERMS,
+    choices: [
+      {
+        name: Choices_Terms.GUESTS,
+        description: Choices_Terms.GUESTS_DESCRIPTION,
+      },
+      { name: Choices_Terms.PETS },
+      { name: Choices_Terms.SMOKING },
+    ],
+  },
+  {
+    sectionName: ChoicesSections.ACCESSABILITY,
+    choices: [
+      {
+        name: Choices_Accessability.CORRIDOR,
+        description: Choices_Accessability.CORRIDOR_DESCRIPTION,
+      },
+      {
+        name: Choices_Accessability.DISABLED,
+        description: Choices_Accessability.DISABLED_DESCRIPTION,
+      },
+    ],
   },
 ];
