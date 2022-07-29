991a01ff93c0941b8861b10016339c19d6c32ae7
VladimirParmon <parmon_vladimir@bk.ru>
2022-07-29 17:12:45 +0300

    change card size and add hover effect

--- src/app/components/card/components.ts
+++ src/app/components/card/components.ts
@@ -1,9 +1,18 @@
 import { styled } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 
-export const CardContainer = styled(Paper)`
-  width: 23%;
-`;
+export const CardContainer = styled(Paper)(
+  ({ theme }) => `
+width: 340px;
+height: 400px;
+cursor: pointer;
+box-sizing: border-box !important;
+
+&:hover {
+  border-bottom: 2px solid ${theme.palette.secondary.main};
+}
+`
+);
 
 export const CardInner = styled("div")`
   padding: 5px 15px 15px 15px;
