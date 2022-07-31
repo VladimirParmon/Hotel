431ae4b7202d5c84661c06e2572f1fb5e0e7afb5
VladimirParmon <parmon_vladimir@bk.ru>
2022-07-31 21:51:14 +0300

    single apartment page responsive layout

---
+++ src/app/pages/singleApartmentPage/components.ts
@@ -0,0 +1,40 @@
+import { styled } from "@mui/material/styles";
+
+export const PageContainer = styled("div")(
+  ({ theme }) => `
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: ${theme.palette.primary.light};
+  flex: 1 1 auto;
+  overflow-x: hidden;
+  height: fit-content;
+  padding: 30px;
+`
+);
+
+export const HugeScreensBoundary = styled("div")`
+  max-width: 1920px;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 30px;
+`;
+
+export const CarouselContainer = styled("div")(
+  ({ theme }) => `
+width: 100%;
+height: auto;
+max-width: 1450px;
+max-height: 600px;
+${theme.breakpoints.down("fullscreen")} {
+  margin-bottom: 70px;
+}
+
+& * {
+  max-height: inherit;
+}
+`
+);
--- src/app/pages/singleApartmentPage/singleApartmentPage.tsx
+++ src/app/pages/singleApartmentPage/singleApartmentPage.tsx
@@ -1,7 +1,34 @@
-import { useLocation } from "react-router-dom";
+import { Typography } from "@mui/material";
+import ApartmentDescription from "app/components/apartmentDescription";
+import BookingWindow from "app/components/bookingWindow";
+import SliderComponent from "app/components/slider";
+import Stars from "app/components/stars";
+import { selectFiltersInfo } from "app/redux/filtersSlice";
+import { useAppSelector } from "app/redux/hooks";
+import { useSingleApartmentInfo } from "app/redux/useSingleApartmentInfo";
+import { useParams } from "react-router-dom";
+import { CarouselContainer, HugeScreensBoundary, PageContainer } from "./components";
 
 export function SingleApartmentPage() {
-  const routerInfo = useLocation();
-  console.log(routerInfo);
-  return <p>Single page works!</p>;
+  const { id } = useParams();
+  const apartment = useSingleApartmentInfo(id);
+  const filters = useAppSelector(selectFiltersInfo);
+
+  if (!apartment) return <p>Page not found</p>;
+
+  return (
+    <PageContainer>
+      <HugeScreensBoundary>
+        <CarouselContainer>
+          <SliderComponent items={apartment.photos} />
+          <span style={{ fontSize: "2rem" }}>Apartment rating:</span>
+          <Stars starSize={30} rating={apartment.rating} />
+        </CarouselContainer>
+        <BookingWindow apartment={apartment} filters={filters} highRes={true} />
+        <ApartmentDescription apartment={apartment}>
+          <BookingWindow apartment={apartment} filters={filters} highRes={false} />
+        </ApartmentDescription>
+      </HugeScreensBoundary>
+    </PageContainer>
+  );
 }
