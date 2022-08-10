3637f0a6e760fe55b4f7b10cef4ae817665a2200
VladimirParmon <parmon_vladimir@bk.ru>
2022-08-10 15:07:26 +0300

    fix: pagination page selection bug

--- src/app/pages/usePagination.ts
+++ src/app/pages/usePagination.ts
@@ -1,7 +1,7 @@
 import { IApartment } from "app/constants/models";
 import { selectFiltersInfo } from "app/redux/filtersSlice";
 import { useAppSelector } from "app/redux/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function usePagination(dataToShow: IApartment[]) {
   const [currentPage, setCurrentPage] = useState(1);
@@ -13,6 +13,10 @@ export function usePagination(dataToShow: IApartment[]) {
     (currentPage - 1) * pageSize,
     (currentPage - 1) * pageSize + pageSize
   );
+
+  useEffect(() => {
+    if (totalPages < currentPage) setCurrentPage(totalPages);
+  }, [totalPages, currentPage]);
 
   return { currentPage, totalPages, currentPageData, setCurrentPage };
 }
